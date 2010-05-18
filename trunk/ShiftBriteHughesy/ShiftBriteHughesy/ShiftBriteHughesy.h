72046ddeee4b6544d9f8a775bd59aaae35d589a0
avrillafuze <avrillafuze@5b0b4009-3fa4-6a40-92f5-ba7605a599a3>
2010-05-18 08:34:12 +0000

    Added new constructor Added new example Random colours

---
+++ trunk/ShiftBriteHughesy/ShiftBriteHughesy/ShiftBriteHughesy.h
@@ -0,0 +1,30 @@
+/*
+	ShiftBriteHughesy.h - A library to run shiftbrites
+	Created by Ashley J. Hughes, 17 May 2010.
+	Based on a post found @ http://macetech.com/blog/node/54
+*/
+#ifndef ShiftBriteHughesy_h
+#define ShiftBriteHughesy_h
+
+#include "WProgram.h"
+
+class ShiftBriteHughesy
+{
+public:
+	ShiftBriteHughesy();
+	ShiftBriteHughesy(int dataPin, int latchPin, int enablePin, int clockPin);
+	void sendColour(int r, int g, int b);
+private:
+	int _SB_CommandMode;
+	int _SB_RedCommand;
+	int _SB_GreenCommand;
+	int _SB_BlueCommand;
+	int _dPin;
+	int _lPin;
+	int _ePin;
+	int _cPin;
+	unsigned long _SB_CommandPacket;
+	void _SB_SendPacket();
+};
+
+#endif
